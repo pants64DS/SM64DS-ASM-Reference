2369012692e123792d1088e45836f914dae3f742
Overblade <57635666+Overblade@users.noreply.github.com>
2019-12-12 00:12:43 +0100

    Added members

--- source/Memory.h
+++ source/Memory.h
@@ -2,33 +2,55 @@
 #define SM64DS_MEMORY_INCLUDED
 
 
+
 struct HPXE
 {
 	const char magic[4];			//HPXE
 	unsigned unk04;
 	unsigned unk08;
-	unsigned unk0C;
-	unsigned unk10;
+	HPXE* nestedHPXE;				//Pointer to nested heap HXPE
+	HPXE* nestedHPXE2;				//Pointer to nested heap after the first one
 	uint16_t unk14;
 	uint16_t unk16;
-	unsigned hpxeEnd;
+	unsigned heapStart;
 	unsigned heapEnd;
-	unsigned align;
-	unsigned unk24;
-	unsigned unk28;
-	unsigned unk2C;
-	unsigned unk30;
+	unsigned unk20;					//Align?
+	MemoryNode* firstFreeBlock;
+	MemoryNode* lastFreeBlock;
+	AllocationNode* firstAllocatedBlock;
+	AllocationNode* lastAllocatedBlock;
 	uint16_t unk34;
 	uint16_t unk36;
 };
 
 
+
+struct AllocationNode
+{
+	unsigned unk00;
+	unsigned size;
+	AllocationNode* previous;
+	AllocationNode* next;
+};
+
+
+struct MemoryNode
+{
+	unsigned unk00;
+	unsigned size;
+	MemoryNode* previous;
+	MemoryNode* next;
+};
+
+
+
+
 //vtable at 0x02099D90, ctor at 0x0203CAAC
 struct Heap								//internal name: mHeap::Heap_t
 {
-	HPXE* hpxePtr;
-	unsigned maxAllocatedBytes;			//First 0x18 bytes excluded (until HPXE)
-	Heap* previousHeap;
+	unsigned memoryBegin;
+	unsigned maxAllocatedBytes;			//Heap area containing HPXE and allocated data
+	Heap* rootHeap;
 	unsigned unk10;
 
 	Heap(unsigned a, unsigned b, unsigned c, unsigned unused);
@@ -40,11 +62,14 @@ struct Heap								//internal name: mHeap::Heap_t
 //vtable at 0x02099DD8, ctor at 0x0203CA80
 struct ExpandingHeap : public Heap		//internal name: mHeap::ExpHeap_t
 {
-	HPXE* hpxePtr2;
-	HPXE heapParameters;
+	HPXE* hpxe;
 
 	ExpandingHeap();
 	virtual ~ExpandingHeap();
+
+	virtual void* Allocate(unsigned size, unsigned addressMask);	//Allocates size bytes with an alignment of addressMask
+	virtual bool Deallocate(void* ptr);								//Deallocates ptr from heap (0x10)
+
 };
 
 //0x020A0EA0 active heap ptr?
