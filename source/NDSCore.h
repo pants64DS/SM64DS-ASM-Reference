903f4e0cd7d9aeeafc17a78ca21ad40f6c7296aa
Overblade <57635666+Overblade@users.noreply.github.com>
2019-12-16 21:37:33 +0100

    Around 50 new symbols

--- source/NDSCore.h
+++ source/NDSCore.h
@@ -12,12 +12,12 @@ namespace CP15 {
 	void MPUDataRegion1(unsigned settings);								//Sets data/unified region 1 MPU settings
 	void MPUDataRegion7(unsigned settings);								//Sets data/unified region 7 MPU settings
 	unsigned MPUGetDataRegion7();										//Returns the MPU's region 7 settings
-	void CleanDataCache();												//Cleans the data cache
-	void CleanAndInvalidateDataCache();									//Cleans and invalidates the data cache
-	void CleanDataCache(unsigned startVA, unsigned size);				//Cleans the data cache (virtual address)
+	void FlushDataCache();												//Flushes the data cache
+	void FlushAndInvalidateDataCache();									//Flushes and invalidates the data cache
+	void FlushDataCache(unsigned startVA, unsigned size);				//Flushes the data cache (virtual address)
 	void InvalidateDataCache(unsigned startVA, unsigned size);			//Invalidates the data cache (virtual address)
 	void InvalidateInstructionCache(unsigned startVA, unsigned size);	//Invalidates the instruction cache (virtual address)
-	void CleanAndInvalidateDataCache(unsigned startVA, unsigned size);	//CLeans and invalidates the data cache (virtual address)
+	void FlushAndInvalidateDataCache(unsigned startVA, unsigned size);	//Flushes and invalidates the data cache (virtual address)
 	void DrainWriteBuffer();											//Finish all bus transactions and end writes
 	void WaitForInterrupt();											//Wait for interrupt
 	void SystemSetup();													//Called right after _start, sets up cache, MPU and memory
@@ -28,8 +28,18 @@ namespace CP15 {
 namespace IRQ {
 
 	typedef void(*IRQHandler)();
-	IRQHandler IRQFunctions[22];				//Contains all IRQ handlers and are indexed by IRQ number
-	IRQHandler DMATIMFunctions[8];				//Contains special DMA/TIM IRQ handlers (4 each) which are preferred over the corresponding irqFunction
+
+	struct DmaTimData {
+		IRQHandler function;					//The extended function to call
+		unsigned unk04;							//one shot if 0?
+		unsigned unk08;
+	};
+
+	static IRQHandler IRQFunctions[22];			//Contains all IRQ handlers and are indexed by IRQ number
+	static uint16_t DmaTimIndices[8];			//Contains the corresponding IRQ bit shift values of DMA and TIM peripherals
+	static DmaTimData DmaTimFunctions[8];		//Contains special DMA/TIM IRQ handlers (4 each) which are preferred over the corresponding irqFunction
+	static unsigned IRQCheckBits;				//Contains the check bits used by BIOS, set by user handler to mark one IRQ as finished
+	static IRQHandler UserIRQAddress;			//Function pointer to UserInterruptHandler, set by _start 
 
 	unsigned Disable();							//Disables interrupts and returns the previous state
 	unsigned Enable();							//Enables interrupts and returns the previous state
@@ -43,19 +53,25 @@ namespace IRQ {
 	unsigned EnableIRQs(unsigned mask);			//Enables interrupts according to mask and returns the previous IE state
 	unsigned SetIRQs(unsigned mask);			//Assigns mask to IE and returns the previous IE state
 	IRQHandler GetIRQHandler(unsigned irq);		//Returns the IRQHandler assigned to the given IRQ number
+	void SetIRQHandler(unsigned mask, IRQHandler function);	//Installs the corresponding IRQ handler to the IRQs given by mask
 
-	
+	void VBlankHandler();						//Called on v-blank
+	void EmptyHandler();						//Called when no IRQ handler is installed
+	void Tim0OverflowHandler();					//Called on TIM0 overflow
+	void Tim1OverflowHandler();					//Called on TIM1 overflow
+	void Tim2OverflowHandler();					//Called on TIM2 overflow
+	void Tim3OverflowHandler();					//Called on TIM3 overflow
+	void Dma0Handler();							//Called on DMA0 interrupt
+	void Dma1Handler();							//Called on DMA1 interrupt
+	void Dma2Handler();							//Called on DMA2 interrupt
+	void Dma3Handler();							//Called on DMA3 interrupt
+	void IPCRxFifoNotEmptyHandler();			//Called when IPC RX FIFO is not empty
+	void GameCardIREQMCHandler();				//Called when an interrupt is asserted on IREQ_MC
+	void DmaTimHandler();						//Called by DMA/TIM(0-3) handlers
+	void UserInterruptHandler();				//Interrupt handler called by BIOS after an interrupt event, address at 0x023C3FFC (setup by _start)
 
 }
 
-
-struct ARMMathState {							//Struct storing math processor state related data
-	uint64_t divNumerator;
-	uint64_t divDenominator;
-	uint64_t sqrtParam;
-	uint16_t divMode;
-	uint16_t sqrtMode;
-};
 
 
 
@@ -66,7 +82,7 @@ namespace cstd {
 	Fix12i fdiv(Fix12i numerator, Fix12i denominator);	//Fix12i division (32 bit)
 	int64_t ldiv(Fix12i numerator, Fix12i denominator); //Fix12i division (64 bit)
 	unsigned sqrt(uint64_t x);							//64 bit unsigned sqrt
-	Fix12i sqrtQ24(Fix12i lower, Fix12i upper) { return Fix12i(sqrt((static_cast<uint64_t>(upper.val) << 32) | lower.val), true); }
+	inline Fix12i sqrtQ24(Fix12i lower, Fix12i upper) { return Fix12i(sqrt((static_cast<uint64_t>(upper.val) << 32) | lower.val), true); }
 
 	int strcmp(const char* str1, const char* str2);		//Compares two strings and returns the difference (0 when equal)
 	char* strncpy(char* dest, const char* src, unsigned count);	//Copies n bytes from src to dest and returns a pointer to dest
@@ -87,22 +103,62 @@ namespace cstd {
 	unsigned atan_table;								//u16 per value, 1024 values
 
 	int atan2(Fix12i y, Fix12i x);						//atan2 function, what about 0x020538b8?
-
 	int abs(int x);										//Returns the absolute value of x
 
 }
 
 
 
+struct ARMMathState {							//Struct storing math processor state related data
+	uint64_t divNumerator;
+	uint64_t divDenominator;
+	uint64_t sqrtParam;
+	uint16_t divMode;
+	uint16_t sqrtMode;
+};
+
+
+struct ARMContext {
+	unsigned cpsr;
+	unsigned r0;
+	unsigned r1;
+	unsigned r2;
+	unsigned r3;
+	unsigned r4;
+	unsigned r5;
+	unsigned r6;
+	unsigned r7;
+	unsigned r8;
+	unsigned r9;
+	unsigned r10;
+	unsigned r11;
+	unsigned r12;
+	unsigned r13;
+	unsigned r14;
+	unsigned targetPC;
+	unsigned swiSP;
+	ARMMathState savedMath;
+};
+
 extern "C" {
 
-	unsigned ARMOperatingMode();				//Returns the current ARM mode bits
+	unsigned ARMProcessorMode();										//Returns the current ARM mode bits
+	bool ARMSaveContext(ARMContext* context);							//Saves the current context, returns 0 if successful
+	void ARMRestoreContext(ARMContext* context);						//Restores the context
+
+	enum IPCStatus{
+		Success = 0,
+		Error = -1,
+		FifoFull = -2
+	};
+
+	IPCStatus IPCSend(unsigned arg0, unsigned arg1, unsigned arg2);		//Transmits data to the ARM7
 
 	void OSReadROMArea(unsigned src, unsigned dest, unsigned size);		//Reads size bytes from the gamecard at src and writes to dest = 0x01FFDBD8
 	//ROMCTRL_SETTINGS = 0x027FFE60
 
-	void ARMMathSaveState(ARMMathState* location);		//Saves the ARM math state = 0x0205E988
-	void ARMMathLoadState(ARMMathState* location);		//Loads the ARM math state = 0x0205E91C
+	void ARMMathSaveState(ARMMathState* location);		//Saves the ARM math state
+	void ARMMathLoadState(ARMMathState* location);		//Loads the ARM math state
 
 	void DMAStartTransferFB(char channel, unsigned src, unsigned dest, unsigned ctrl);	//Starts DMA Transfer and waits two load cycles
 	void DMAStartTransfer(char channel, unsigned src, unsigned dest, unsigned ctrl);	//Starts DMA Transfer
