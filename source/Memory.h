5e7660e5bb7eee9cb0b177713936d4c5cb52aa2a
Overblade <57635666+Overblade@users.noreply.github.com>
2019-12-19 22:58:07 +0100

    Heap decoded: ~42%

--- source/Memory.h
+++ source/Memory.h
@@ -21,21 +21,42 @@ struct HPXE
 	MemoryNode* lastAllocatedBlock;
 	uint16_t unk34;
 	uint16_t unk36;
+
+	void* Allocate(unsigned size, unsigned align);			//Allocates memory by calling AllocateMemory and returns a ptr to it. Size needn't be a multiple by four since it gets aligned before the call.
+	void* AllocateMemory(unsigned size, unsigned align);	//Tries to find an address to fit the allocation aligned at align bytes. If enough space is available, it returns a pointer to the newly allocated memory. If not, it returns a nullptr.
+	bool Deallocate(void* ptr);								//Deallocates, returns 1 if successful
+	unsigned Reallocate(void* ptr, unsigned newSize);		//Reallocates and returns the final node size. If newSize is less than the previous size, a part of ptr is freed; if newSize is greater than the previous size, ptr may change in case the new size doesn't fit into the old memory block.
+	unsigned MemoryLeft();									//Returns the total number of free bytes
+	unsigned MaxAllocatableSize(unsigned align);			//Returns the size of the biggest contiguous free memory block aligned with least alignment padding (if multiple have the same size)
+
 };
 
+
+//0x020A4D38
+struct UnkStruct
+{
+	unsigned unk00;
+	unsigned unk04;
+	uint16_t unk08;
+	uint16_t unk0a;
+};
+
+static unsigned Func_0203C8E8(unsigned heapTarget, unsigned size);
 
 
 struct MemoryNode
 {
-	struct TargetInfo				//Allocated on stack
+	struct TargetInfo
 	{
 		unsigned start;
 		unsigned end;
+
+		TargetInfo(const MemoryNode& node);		//Creates a TargetInfo from the node's parameters
 	};
 
-	char magic[2];					//DU or RF
+	char magic[2];								//DU (allocated) or RF (free)
 	uint16_t unk02;
-	unsigned size;
+	unsigned size;								//Size not including node
 	MemoryNode* previous;
 	MemoryNode* next;
 };
@@ -56,6 +77,13 @@ struct Heap								//internal name: mHeap::Heap_t
 	Heap(unsigned a, unsigned b, unsigned c, unsigned unused);
 	virtual ~Heap();
 
+	static MemoryNode* LinkNode(MemoryNode* hpxeFLNodePair, MemoryNode* newNode, MemoryNode* prevNode);			//Link node on heap (corrects previous and next) and return a pointer to the new node
+	static MemoryNode* UnlinkNode(MemoryNode* hpxeFLNodePair, MemoryNode* target);								//Unlink node from heap (corrects previous and next) and return a pointer to the node before the unlinked one
+	static MemoryNode* CreateNode(MemoryNode::TargetInfo* targetInfo, char* nodeType);							//Sets up a new node at targetInfo.start, zeroes it and stores the block size in it. Returns a pointer to the newly created node.
+	static void* AllocateNode(MemoryNode* hpxeFreeNodePair, MemoryNode* freeNode, unsigned address, unsigned size, unsigned arg4);	//Allocates size bytes at address (and zeroes it) inside freeNode, fixes node links and returns a ptr to the data of the new node after setup
+	static unsigned SizeofInternal(void* ptr);																	//Returns the allocated size of a generic pointer to an allocated memory block
+	static bool FreeNode(MemoryNode* hpxeFreeNodePair, MemoryNode::TargetInfo* targetInfo);						//Frees the node and merges free blocks. Returns 1 if deallocation has been successful, 0 otherwise.
+
 };
 
 
@@ -67,24 +95,14 @@ struct ExpandingHeap : public Heap		//internal name: mHeap::ExpHeap_t
 	ExpandingHeap();
 	virtual ~ExpandingHeap();
 
-	virtual void* Allocate(unsigned size, unsigned addressMask);												//Allocates size bytes with an alignment of addressMask
-	virtual bool Deallocate(void* ptr);																			//Deallocates ptr from heap (0x10)
-	virtual unsigned GetSize(void* ptr);																		//Returns the size of an allocated block
-
-	static MemoryNode* linkNode(MemoryNode* hpxeFLNodePair, MemoryNode* newNode, MemoryNode* prevNode);			//Link node on heap (corrects previous and next) and return a pointer to the new node
-	static MemoryNode* unlinkNode(MemoryNode* hpxeFLNodePair, MemoryNode* target);								//Unlink node from heap (corrects previous and next) and return a pointer to the node before the unlinked one
-	static MemoryNode* createNode(MemoryNode::TargetInfo* targetInfo, char* nodeType);							//Sets up a new node at targetInfo.start, zeroes it and stores the block size in it. Returns a pointer to the newly created node.
-	static unsigned AllocatedSize(void* ptr);																	//Returns the allocated size of a generic pointer to an allocated memory block
+	virtual void* Allocate(unsigned size, int addressMask);												//Allocates size bytes with an alignment of addressMask
+	virtual bool Deallocate(void* ptr);																	//Deallocates ptr from heap
+	virtual unsigned Sizeof(void* ptr);																	//Returns the size of an allocated block
+	virtual unsigned Reallocate(void* ptr, unsigned newSize);											//Reallocates the memory given by ptr with the size of newSize
+	virtual unsigned MemoryLeft();																		//Returns the number of unallocated bytes
+	virtual unsigned MaxAllocatableSize();																//Returns the size of the largest contiguous free memory block
 
 };
-
-//0x0204E8E0 linkNode
-//0x0204E910 unlinkNode
-//0x0204E8B0 createNode
-//0x0204E084 AllocatedSize
-
-//0x020A0EA0 active heap ptr?
-//special struct at +0x18
 
 
 //vtable at 0x02099D48, ctor at 0x0203CA54
